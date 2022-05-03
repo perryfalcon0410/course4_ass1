cd9b9e941abe57a78fff26df9018e4f8e183d24d
Huỳnh Kim Hưng <infernoid0410@gmail.com>
2022-05-04 04:19:12 +0700

    commit1

--- plot4.R
+++ plot4.R
@@ -18,7 +18,7 @@ with(data, {
        ylab="Energy sub metering", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power~Datetime,type='l',ylab='Global_reactive_power',xlab='datetime')
   
